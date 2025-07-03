33e0204471af155d209b675abed30552f9676e13
getpoorpoo <getpoorpoo@gmail.com>
2025-07-03 23:11:10 +0900

    ✨ Feat: BottomTabBar 구현

--- src/components/common/BottomTabBar.tsx
+++ src/components/common/BottomTabBar.tsx
@@ -1,5 +1,40 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+import homeIcon from "../../assets/bottom/icon-bottom-home.svg";
+import mapIcon from "../../assets/bottom/icon-bottom-map.svg";
+import profileIcon from "../../assets/bottom/icon-bottom-profile.svg";
+import writeIcon from "../../assets/bottom/icon-bottom-write.svg";
+
 const BottomTabBar = () => {
-  return <></>;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const visiblePaths = ["/", "/map", "/record/new", "/mypage"];
+  if (!visiblePaths.includes(location.pathname)) return null;
+
+  const tabs = [
+    { path: "/", icon: homeIcon, label: "홈" },
+    { path: "/map", icon: mapIcon, label: "지도" },
+    { path: "/record/new", icon: writeIcon, label: "작성" },
+    { path: "/mypage", icon: profileIcon, label: "마이" }
+  ];
+
+  return (
+    <div className="w-full h-[60px] bg-white flex">
+      {/* 탭바 wrapper */}
+      <div className="w-full max-w-[375px] border-t-[2px] border-[#999999] bg-white flex h-[60px]">
+        {tabs.map((tab) => (
+          <button
+            key={tab.path}
+            onClick={() => navigate(tab.path)}
+            className="flex flex-1 flex-col items-center justify-center"
+          >
+            <img src={tab.icon} alt={tab.label} className="w-6 h-6" />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BottomTabBar;
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,29 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import BottomTabBar from "../components/common/BottomTabBar";
 import Header from "../components/common/Header";
 
 const Layout = () => {
+  const location = useLocation();
+  const visiblePaths = ["/", "/map", "/record/new", "/mypage"];
+  const showBottomTabBar = visiblePaths.includes(location.pathname);
+
   return (
-    <div className="w-full min-h-screen bg-gray-100 flex justify-center items-center">
+    <div className="w-full min-h-screen flex justify-center bg-gray-100">
       <div
-        className="w-full max-w-[375px] bg-white flex flex-col shadow-lg rounded-xl overflow-hidden"
+        className="flex flex-col w-full max-w-[375px] bg-white shadow-lg"
         style={{ height: "calc(var(--vh, 1vh) * 100)" }}
       >
-        {/* 상단 바 */}
-        <div className="flex-none h-[56px] shrink-0">
+        {/* 상단 헤더 */}
+        <div className="h-[56px] flex-none">
           <Header />
         </div>
 
-        {/* 페이지 콘텐츠 */}
+        {/* 메인 콘텐츠 */}
         <main className="flex-1 overflow-auto">
           <Outlet />
         </main>
 
-        {/* 하단 탭 바 */}
-        <div className="flex-none h-[64px] shrink-0">
-          <BottomTabBar />
-        </div>
+        {/* 하단 탭바 */}
+        {showBottomTabBar && (
+          <div className="h-[60px] flex-none">
+            <BottomTabBar />
+          </div>
+        )}
       </div>
     </div>
   );
