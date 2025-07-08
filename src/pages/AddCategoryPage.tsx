b4c00abb68db7409a81cbfae95d06643722e0bd2
choyekyo <qkdxksxoxo@naver.com>
2025-07-08 19:43:18 +0900

    ✨ Feat: 카테고리 추가 페이지 및 lucide react 추가

---
+++ src/pages/AddCategoryPage.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import Header from "../components/common/Header";
+import { Check } from "lucide-react";
+
+const categoryColors = [
+    "#FB8A1F",
+    "#FFDC5D",
+    "#77B255",
+    "#4289C1",
+    "#BB66CF",
+    "#E0A1A1",
+    "#DF87C7"
+
+]
+
+function AddCategoryPage() {
+    const [categoryName, setCategoryName] = useState(""); 
+    const [categoryColor, setCategoryColor] = useState<string | null>(null); 
+
+    const isFormValid = categoryName.trim() !== "" && categoryColor !== null;
+
+    return (
+        <>
+            <Header title="새 카테고리 추가" underline={true} />
+            <div className="flex flex-col px-10 py-6 justify-center">
+                <input 
+                    type="text" 
+                    value={categoryName} 
+                    onChange={(e) => setCategoryName(e.target.value)}
+                    placeholder="새 카테고리명을 입력해주세요"
+                    className="border-b px-1 py-2 outline-none"
+                />
+                <div className="py-5">
+                    <div className="text-sm">색상 선택</div>
+                    <div className="flex flex-c">
+                        {categoryColors.map((color) => (
+                            <button
+                                key={color}
+                                onClick={() => setCategoryColor(color)}
+                                style={{ backgroundColor: color }}
+                                className="category-color-button"
+                            >
+                                {categoryColor === color && (
+                                    <div className="absolute inset-0 flex items-center justify-center">
+                                        <Check className="w-4 h-4 text-black" />
+                                    </div>
+                                )}
+                            </button>
+                        ))}
+                    </div>
+                    <button 
+                        disabled={!isFormValid}
+                        className={`mt-80 w-70 h-10 ${isFormValid ? "bg-amber-300 hover:bg-amber-400 cursor-pointer" : "bg-gray-300 cursor-not-allowed"}`}>완료</button>
+                </div>
+
+            </div>
+
+        </>
+    );
+}
+export default AddCategoryPage;
