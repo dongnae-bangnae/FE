01654d47b8c2eebac9ea4cea532e1ae7704a493b
getpoorpoo <getpoorpoo@gmail.com>
2025-07-28 01:05:18 +0900

    🔧 Chore: 전역 Content-Type 설정 제거

--- src/apis/member.ts
+++ src/apis/member.ts
@@ -1,8 +1,14 @@
 import { axiosInstance } from "./axiosInstance";
 
 export const patchNickname = async (nickname: string) => {
-  const response = await axiosInstance.patch("/api/member/nickname", {
-    nickname
-  });
+  const response = await axiosInstance.patch(
+    "/member/nickname",
+    { nickname },
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
   return response.data;
 };
