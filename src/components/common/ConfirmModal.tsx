65a39d5d2d2a016084b31eb72f7aabeacfd8010c
choyekyo <qkdxksxoxo@naver.com>
2025-07-28 22:02:10 +0900

    ✨ Feat: 게시글 페이지로 props 전달 외 UI 수정 (#93)

--- src/components/common/ConfirmModal.tsx
+++ src/components/common/ConfirmModal.tsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface ConfirmModalProps {
   message: React.ReactNode;
-  onConfirm: () => void;
+  onConfirm: (placeName: string) => void;
   onCancel: () => void;
+  defaultValue?: string; 
+  requiredInput?: boolean;
 }
 
-const ConfirmModal = ({ message, onConfirm, onCancel }: ConfirmModalProps) => {
+const ConfirmModal = ({ message, onConfirm, onCancel, defaultValue="", requiredInput=false }: ConfirmModalProps) => {
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+		setInputValue(defaultValue);
+	}, [defaultValue]);
+
+  const handleConfirmClick = () => {
+		if (requiredInput && inputValue.trim() === "") {
+			alert("장소명을 입력해 주세요!");
+			return;
+		}
+		onConfirm(inputValue.trim());
+	};
+
   return (
     <div className="fixed inset-0 bg-[#D9D9D9]/50 flex items-center justify-center z-50">
-      <div className="bg-[#FFC064] px-6 py-10 rounded-xl text-center w-[260px]">
-        <p className="text-sm font-semibold mb-6 text-black">{message}</p>
+      <div className="bg-[#D9D9D9] px-6 py-6 rounded-xl text-center w-[260px]">
+        <p className="text-sm  mb-4 text-black">{message}</p>
+        <input type='text' value={inputValue} onChange={(e) => setInputValue(e.target.value)} 
+                className="bg-white mb-3 h-10 w-50 focus:outline-none pl-3
+                            placeholder-[#FF6A00] placeholder:text-sm placeholder:p-12
+                            text-sm" placeholder="장소명 입력"/>
         <div className="flex justify-between gap-3">
           <button
             onClick={onCancel}
@@ -19,7 +39,7 @@ const ConfirmModal = ({ message, onConfirm, onCancel }: ConfirmModalProps) => {
             취소
           </button>
           <button
-            onClick={onConfirm}
+            onClick={handleConfirmClick}
             className="flex-1 bg-white text-black py-2 rounded-md text-sm font-medium hover:bg-[#FF9700]"
           >
             네
--- src/pages/SelectPinTypePage.tsx
+++ src/pages/SelectPinTypePage.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/common/Header";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import iconArts from "../assets/place/place-arts.svg";
 import iconBar from "../assets/place/place-bar.svg";
@@ -14,14 +14,14 @@ import iconWalk from "../assets/place/place-walk.svg";
 import CheckIcon from "../assets/icon-checked.svg?react";
 
 const categories = [
-	{ id: "food", label: "맛집", color: "#FDF3F7", border: "#E94E77", icon: iconFood },
-	{ id: "cafe", label: "카페", color: "#F3F0FF", border: "#7B61FF", icon: iconCafe },
-	{ id: "bar", label: "술집", color: "#E8F3FF", border: "#419DCE", icon: iconBar },
-	{ id: "walk", label: "산책", color: "#E9F8EF", border: "#498C6D", icon: iconWalk },
-	{ id: "sports", label: "운동", color: "#F4F9FF", border: "#005B9D", icon: iconSports },
-	{ id: "books", label: "서점", color: "#F1F1F1", border: "#333333", icon: iconBooks },
-	{ id: "arts", label: "문화 예술", color: "#FDF4EA", border: "#C7763E", icon: iconArts },
-	{ id: "others", label: "기타", color: "#E4E4E4", border: "#444444", icon: iconOthers },
+	{ id: "FOOD", label: "맛집", color: "#FDF3F7", border: "#E94E77", icon: iconFood },
+	{ id: "CAFE", label: "카페", color: "#F3F0FF", border: "#7B61FF", icon: iconCafe },
+	{ id: "PUB", label: "술집", color: "#E8F3FF", border: "#419DCE", icon: iconBar },
+	{ id: "WALK", label: "산책", color: "#E9F8EF", border: "#498C6D", icon: iconWalk },
+	{ id: "EXERCISE", label: "운동", color: "#F4F9FF", border: "#005B9D", icon: iconSports },
+	{ id: "BOOKSTORE", label: "서점", color: "#F1F1F1", border: "#333333", icon: iconBooks },
+	{ id: "CULTURE_ART", label: "문화 예술", color: "#FDF4EA", border: "#C7763E", icon: iconArts },
+	{ id: "ETC", label: "기타", color: "#E4E4E4", border: "#444444", icon: iconOthers },
 ];
 
 function SelectedPinTypePage() {
@@ -29,12 +29,19 @@ function SelectedPinTypePage() {
 	const [selectedPlace, setSelectedPlace] = useState<string | null>(null);
 	const [hoveredPlace, setHoveredPlace] = useState<string | null>(null);
 
+	const location = useLocation();
+	const { latitude, longitude, placeName } = location.state || {};
+
 	const handleComplete = () => {
-		if (!selectedPlace) {
-			alert("카테고리를 선택해주세요!");
-			return;
-		}
-		navigate("/record/:id");
+		if (!selectedPlace) return;
+		navigate("/record/new/write", {
+			state: {
+				latitude,
+				longitude,
+				placeName,
+				pinCategory: selectedPlace,
+			}, 
+		});
 	};
 
 	return (
@@ -45,6 +52,7 @@ function SelectedPinTypePage() {
 				right={
 					<button
 						onClick={handleComplete}
+						disabled={!selectedPlace}
 						className={`whitespace-nowrap mr-4 px-4 py-2 text-sm font-medium rounded-md shadow-md
 							${selectedPlace ? "bg-[#FFAC33] hover:bg-amber-500 cursor-pointer" : "bg-gray-200 cursor-not-allowed"}`}>
 						완료
@@ -70,7 +78,7 @@ function SelectedPinTypePage() {
 							}}
 						>
 							<div className="flex items-center gap-3">
-								<img src={icon} alt={label} className={`${id === "walk" ? "w-7 h-7" : "w-6 h-6"}`}/>
+								<img src={icon} alt={label} className={`${id === "WALK" ? "w-7 h-7" : "w-6 h-6"}`}/>
 								<span className="font-normal">{label}</span>
 							</div>
 
