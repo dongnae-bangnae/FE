ec99eeec213e55d2a7b76675793263c9af7a1528
dongyeonkim1 <dongyeon8136@naver.com>
2025-07-31 02:07:25 +0900

    💄Design: 답글 ui 수정(#10)

---
+++ src/components/Record/CommentItem.tsx
@@ -0,0 +1,89 @@
+import MenuBarIcon from "../../assets/record/icon-menubar.svg";
+import DefaultProfileIcon from "../../assets/icon-defaultProfile.svg";
+import ResponseIcon from "../../assets/record/icon-comment.svg";
+import fonts from "../../styles/fonts";
+
+interface CommentItemProps {
+  nickname: string;
+  content: string;
+  showReplyButton?: boolean;
+  onReplyClick?: () => void;
+  children?: React.ReactNode;
+  isReply?: boolean;
+}
+
+const CommentItem = ({
+  nickname,
+  content,
+  showReplyButton = true,
+  onReplyClick,
+  children,
+  isReply = false,
+}: CommentItemProps) => {
+  return (
+    <div
+      className="border rounded-xl w-full max-w-[355px]"
+      style={{
+        border: "1px solid rgba(0, 0, 0, 0.47)",
+        marginTop: isReply ? "10px" : 0,
+      }}
+    >
+      {/* 프로필 + 닉네임 */}
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2 mb-2 ml-2 mt-2">
+          {isReply && (
+            <img src={ResponseIcon} alt="response" className="w-[18px] h-[18px]" />
+          )}
+          <img
+            src={DefaultProfileIcon}
+            alt="avatar"
+            className="w-[25px] h-[25px] rounded-full"
+          />
+          <span
+            className="text-sm"
+            style={{
+              fontSize: fonts.size.caption,
+              fontWeight: fonts.weight.regular,
+            }}
+          >
+            @{nickname}
+          </span>
+        </div>
+        <button className="mr-[12px]">
+          <img src={MenuBarIcon} />
+        </button>
+      </div>
+
+      <div className="w-full border-b border-[#999999] mb-2" />
+
+      {/* 본문 + 답글버튼 */}
+      <div className="flex items-start gap-2 ml-2 mb-2">
+        {showReplyButton && (
+          <button
+            onClick={onReplyClick}
+            style={{
+              backgroundColor: "#FFAC33",
+              fontSize: "12px",
+              fontWeight: fonts.weight.regular,
+              border: "none",
+              borderRadius: "10px",
+              cursor: "pointer",
+              width: "46px",
+              height: "27px",
+              flexShrink: 0,
+            }}
+          >
+            답글
+          </button>
+        )}
+        <p className="text-sm break-words w-full" style={{ fontSize: "13px" }}>
+          {content}
+        </p>
+      </div>
+
+      {children}
+    </div>
+  );
+};
+
+export default CommentItem;
