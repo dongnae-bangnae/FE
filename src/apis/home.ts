8337e4fb18993a158b22e2367f9b8ab0021f3d9f
Dohee Kwon <doeesoioi3@gmail.com>
2025-08-11 15:43:36 +0900

    Fix: 온보딩 및 홈 경로 수정

--- src/apis/home.ts
+++ src/apis/home.ts
@@ -2,7 +2,7 @@ import { axiosInstance } from "./axiosInstance";
 
 // 홈 화면 새 글 리스트 조회 API (page=1 기본값)
 export const getNewArticles = async (page: number = 1) => {
-  const response = await axiosInstance.get("/home/articles", {
+  const response = await axiosInstance.get("/api/home/articles", {
     params: { page }
   });
 
@@ -11,7 +11,9 @@ export const getNewArticles = async (page: number = 1) => {
 
 // 챌린지 상세 정보 조회 API
 export const getChallengeDetail = async (challengeId: string) => {
-  const response = await axiosInstance.get(`/home/challenges/${challengeId}`);
+  const response = await axiosInstance.get(
+    `/api/home/challenges/${challengeId}`
+  );
 
   return response.data.result;
 };
--- src/apis/onboarding.ts
+++ src/apis/onboarding.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "./axiosInstance";
 
 export const postOnboarding = async (data: FormData) => {
-  const res = await axiosInstance.post("api/member/onboarding", data);
+  const res = await axiosInstance.post("/api/member/onboarding", data);
   return res.data;
 };
