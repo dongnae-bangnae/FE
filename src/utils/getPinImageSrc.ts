c0954bd7726eb05c0e14613bc3ca8918832de538
choyekyo <qkdxksxoxo@naver.com>
2025-08-15 21:46:30 +0900

    💄Design: 핀 에셋 교체 (#203)

--- src/utils/getPinImageSrc.ts
+++ src/utils/getPinImageSrc.ts
@@ -1,13 +1,22 @@
+import pinFood from "../assets/pin/pin_food.svg";
+import pinCafe from "../assets/pin/pin_cafe.svg";
+import pinPub from "../assets/pin/pin_pub.svg";
+import pinWalk from "../assets/pin/pin_walk.svg";
+import pinExercise from "../assets/pin/pin_sports.svg";
+import pinBookstore from "../assets/pin/pin_bookstore.svg";
+import pinArts from "../assets/pin/pin_culture_art.svg";
+import pinETC from "../assets/pin/pin_etc.svg";
+
 export const getPinImageSrc = (category: string): string => {
 	const map: Record<string, string> = {
-		FOOD: "/src/assets/pin/pin_food.svg",
-		CAFE: "/src/assets/pin/pin_cafe.svg",
-		PUB: "/src/assets/pin/pin_pub.svg",
-		WALK: "/src/assets/pin/pin_walk.svg",
-		EXERCISE: "/src/assets/pin/pin_sports.svg",
-		BOOKSTORE: "/src/assets/pin/pin_bookstore.svg",
-		CULTURE_ART: "/src/assets/pin/pin_culture_art.svg",
-		ETC: "/src/assets/pin/pin_etc.svg",
+		FOOD: pinFood,
+		CAFE: pinCafe,
+		PUB: pinPub,
+		WALK: pinWalk,
+		EXERCISE: pinExercise,
+		BOOKSTORE: pinBookstore,
+		CULTURE_ART: pinArts,
+		ETC: pinETC,
 	};
 	return map[category] ?? map["ETC"];
 };
