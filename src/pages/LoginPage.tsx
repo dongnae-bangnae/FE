c623a802f4d5eb24c0aa0181f57fa89eefc50074
Dohee Kwon <doeesoioi3@gmail.com>
2025-08-21 17:04:05 +0900

    ✨ Feat: 로그인 및 OAuth에 로딩스피너 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,10 +5,12 @@ import googleIcon from "../assets/icon-google.svg";
 import kakaoIcon from "../assets/icon-kakao.svg";
 import naverIcon from "../assets/icon-naver.svg";
 import logo from "../assets/logo2.svg";
+import OnBoardingLoadingSpinner from "../components/OnBoardingLoadingSpinner";
 
 function LoginPage() {
   const location = useLocation();
   const [toastMessage, setToastMessage] = useState("");
+  const [authLoading, setAuthLoading] = useState(false);
 
   const handleSocialLogin = (provider: "naver" | "kakao" | "google") => {
     const loginUrls = {
@@ -18,13 +20,20 @@ function LoginPage() {
     };
 
     const redirectUrl = loginUrls[provider];
-
-    if (redirectUrl) {
-      window.location.href = redirectUrl;
-    } else {
+    if (!redirectUrl) {
       console.error(`Login URL for ${provider} is not defined`);
+      return;
     }
+
+    setAuthLoading(true); // 스피너 켜기
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        window.location.href = redirectUrl;
+      });
+    });
   };
+
+  if (authLoading) return <OnBoardingLoadingSpinner />;
 
   useEffect(() => {
     if (location.state?.message) {
